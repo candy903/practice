5b0238d7b8a85b879e874672fd3743e8a11246c7
hxy <1617422254@qq.com>
2018-12-03 16:32:50 +0800

    监听

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var mincss = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var server = require('gulp-webserver');
+var rev = require('gulp-rev');
+var collector = require('gulp-rev-collector');
 
 //起服务
 // gulp.task('devserver', function() {
@@ -16,21 +18,23 @@ var server = require('gulp-webserver');
 
 //压缩css
 gulp.task('devscss', function() {
-    return gulp.src('./src/scss/index.scss')
+    return gulp.src('./src/scss/*.scss')
         .pipe(sass())
         .pipe(mincss())
+        .pipe(concat('all.css'))
         .pipe(gulp.dest('./src/css'))
 })
 
 gulp.task('devjs', function() {
     return gulp.src('./src/js/*.js')
+        .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./src/js/minjs'))
 })
 
-// gulp.task('watch', function() {
-//     return gulp.watch('./src/scss/index.scss', gulp.series('devscss'))
-//     gulp.watch('./src/js/index.js', gulp.series('devjs'))
-// })
+gulp.task('watch', function() {
+    gulp.watch('./src/scss/*.scss', gulp.series('devscss'))
+    gulp.watch('./src/js/*.js', gulp.series('devjs'))
+})
 
 // gulp.task('dev', gulp.series('devscss', 'devjs', 'devserver', 'watch'))
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,5 @@ console.log(000000)
 var a = 10;
 
 var b = 90;
+
+var c = 80;
